88c3fe7ae8e4ba10d027bfcddb8133d351258ad2
ricardo-jnani <rjg.jnani@gmail.com>
2017-08-21 14:18:42 -1000

    Add files via upload

--- jnani_photography/js/main.js
+++ jnani_photography/js/main.js
@@ -9,7 +9,17 @@
     //navbar stops, becomes fixed at top of page
 
 // SHOW NAVIGATION ICONS / BUTTONS
-  //When the user scrolls down past ~ 1000px
+$(window).on('scroll', function (){
+  var distanceScrolled = $(window).scrollTop();
+  console.log('The distance scrolled in:' + distanceScrolled);
+
+  if (distanceScrolled > 1500){
+    $('#up, #down, #view-index').fadeIn(1000);
+  } else {
+    $('#up, #down, #view-index').fadeOut(1000);
+  }
+});
+  //When the user scrolls down past ~ 1000px (look at Sarah's solution file >> scroll by pixels)
     //Show navigation Icons (Up & Down Arrows) fixed on right side of page
     //If current view style is Default (Slide View)
       //Show Index View Icon
@@ -26,18 +36,34 @@
     //Else make currentImageNumber = numberImages
 
 // INDEX VIEW ICON
+$('#view-index').on('click', function (e) {
+  e.preventDefault();
+  console.log('TURN TO INDEX-VIEW!');
+
+  $('#iceland, #maui').addClass('index-view');
+  $('#view-slide').fadeIn(300);
+  $('#view-index').hide(300);
+});
   //When the user clicks on Index View Icon
     //Add Class index-view-style (changing flexbox to have flex-basis=22% & space-between) to all slides
     //Hide Index View Icon
     //Show Slide View Icon
 
 // SLIDE VIEW ICON
+$('view-slide').on('click', function (e){
+  e.preventDefault();
+  console.log('TURN TO SLIDE-VIEW!');
+
+  $('#iceland, #maui').removeClass('index-view');
+  $('')
+});
   //When the user clicks on Slide View Icon
     //Add Class slide-view-style (changing flexbox to have flex-basis=22% & space-between) to all slides
     //Hide Slide View Icon
     //Show Index View Icon
 
 //SMOOTH SCROLL
-
+  // google jQuery anchor smooth scroll
 
 //FILTER CONTENT
+  //similar to how we filtered the author paragraph in the madlibs lab
